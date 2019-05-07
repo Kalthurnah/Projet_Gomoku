84810afb8c4eb4887b704767c825862b522ef8bd
Amine A <amine-98@live.fr>
2019-05-07 09:58:39 +0200

    Fonction "demander_couleur" et base du projet

--- Projet_Gomoku.py
+++ Projet_Gomoku.py
@@ -3,7 +3,24 @@
 Projet IA, Gomoku, Groupe TD A
 @author: Damien ALOUGES, Amine AGOUSSAL, Cécile AMSALLEM
 """
+vide_char = 0
+def Gomoku():
+    (IA_char,user_char)=demander_couleur()
 
+
+def demander_couleur():
+    print("Les noirs commencent. Veux tu être :")
+    print("1 - Les noirs")
+    print("2 - Les blancs")
+    choix = input(">")
+
+    if choix == 1:
+        user_char= 1
+        IA_char= 2
+    else:
+        user_char = 2
+        IA_char = 1
+    return (IA_char,user_char)
 
 
 if __name__ == '__main__':
