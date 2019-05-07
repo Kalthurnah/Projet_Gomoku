51dc5ed9b3fab9fdead7009131e2cfbc5fb9fe13
Amine A <amine-98@live.fr>
2019-05-07 09:52:00 +0200

    Fichier de tests, vide

---
+++ Tests_Gomoku.py
@@ -0,0 +1,8 @@
+from Projet_Gomoku import *
+
+
+def test_vide():
+    # Exemple de test unitaire, à supprimer
+    #Pour plus d'infos, cf pytest
+    assert 0 == 0
+    # assert distance([1,3],[4,3])==3
