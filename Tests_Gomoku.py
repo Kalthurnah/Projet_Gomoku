20f1420aeed4456ed843529660bdb76d0e48373f
Amine A <amine-98@live.fr>
2019-05-07 11:20:31 +0200

    Supprimé test_vide

--- Tests_Gomoku.py
+++ Tests_Gomoku.py
@@ -1,14 +1,6 @@
 from Projet_Gomoku import *
 import numpy as np
 
-
-def test_vide():
-    # Exemple de test unitaire, à supprimer
-    # Pour plus d'infos, cf pytest
-    assert 0 == 0
-    # assert distance([1,3],[4,3])==3
-
-
 def test_grille_complete():
     # Complète = False, a_gagne = 0
     grille_zeros = np.zeros((15, 15), int)
