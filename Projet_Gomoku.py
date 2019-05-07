9936645755a5604c1a08855e99ff20519219fe1c
Amine A <amine-98@live.fr>
2019-05-07 09:45:45 +0200

    Premier commit

---
+++ Projet_Gomoku.py
@@ -0,0 +1,11 @@
+# -*- coding utf-8 -*-
+"""
+Projet IA, Gomoku, Groupe TD A
+@author: Damien ALOUGES, Amine AGOUSSAL, Cécile AMSALLEM
+"""
+
+
+
+if __name__ == '__main__':
+    # Appeler main ici
+    exit()
