27bea65dc2a5fe6bb67f86211fc49908f7b6c077
Amine A <amine-98@live.fr>
2019-05-07 10:24:58 +0200

    Corrigé demander_couleurs et Gomoku

--- Projet_Gomoku.py
+++ Projet_Gomoku.py
@@ -4,9 +4,6 @@ Projet IA, Gomoku, Groupe TD A
 @author: Damien ALOUGES, Amine AGOUSSAL, Cécile AMSALLEM
 """
 vide_char = 0
-def Gomoku():
-    (IA_char,user_char)=demander_couleur()
-
 
 def demander_couleur():
     print("Les noirs commencent. Veux tu être :")
@@ -14,15 +11,19 @@ def demander_couleur():
     print("2 - Les blancs")
     choix = input(">")
 
-    if choix == 1:
-        user_char= 1
-        IA_char= 2
+    if choix == "1":
+        user_char = 1
+        IA_char = 2
     else:
         user_char = 2
         IA_char = 1
-    return (IA_char,user_char)
+    return (IA_char, user_char)
 
+def Gomoku():
+
+    print(user_char)
 
 if __name__ == '__main__':
     # Appeler main ici
+    (IA_char, user_char) = demander_couleur()
     Gomoku()
