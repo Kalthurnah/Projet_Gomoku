304d6f915a4c9a7c377b7a01ca15aded7ac24eb3
Amine A <15179425+AmineI@users.noreply.github.com>
2019-05-09 18:07:22 +0200

    Squelette de la fonction afficher_plateau

--- Projet_Gomoku.py
+++ Projet_Gomoku.py
@@ -11,6 +11,16 @@ user_char = None
 IA_char = None
 
 
+def afficher_plateau(grille):
+    '''
+    Fonction affichant un plateau de Gomoku
+
+    :param grille: grille numpy du plateau à afficher
+    '''
+    # TODO
+    print()
+
+
 def conversion_pos_coord(position: str):
     '''
     Convertit une position entrée par l'utilisateur sous la forme "A4" en un tuple coordonnées d'une grille, sous la forme (0,3)
