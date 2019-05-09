f2d01d1284d3ad25e6f84845e6720b35f6613a2d
Amine A <amine-98@live.fr>
2019-05-09 04:51:38 +0200

    Suppression du test vide exemple, et auto-indentation

--- test_Gomoku.py
+++ test_Gomoku.py
@@ -1,22 +1,18 @@
 from Projet_Gomoku import *
 import numpy as np
 
-def test_vide():
-    # Exemple de test unitaire, à supprimer
-    #Pour plus d'infos, cf pytest
-    assert 0 == 0
-    # assert distance([1,3],[4,3])==3
-    
+
 def test_verif_tour3():
-    grille = np.zeros((15,15),int)
+    grille = np.zeros((15, 15), int)
     grille[1][3] = 2
     grille[7][7] = 1
-    assert verif_tour3(grille, (1,3)) == False
-    assert verif_tour3(grille,(6,4)) == False
-    assert verif_tour3(grille,(0,0)) == True
+    # Grille au 3e tour
+    assert verif_tour3(grille, (1, 3)) == False
+    assert verif_tour3(grille, (6, 4)) == False
+    assert verif_tour3(grille, (0, 0)) == True
 
-    
+
 def test_conversion_pos_coord():
-    assert conversion_pos_coord(("A",4)) == (0,3)
-    assert conversion_pos_coord(("B",7)) == (1,6)
-    assert conversion_pos_coord(("O",1)) == (14,0)
+    assert conversion_pos_coord(("A", 4)) == (0, 3)
+    assert conversion_pos_coord(("B", 7)) == (1, 6)
+    assert conversion_pos_coord(("O", 1)) == (14, 0)
