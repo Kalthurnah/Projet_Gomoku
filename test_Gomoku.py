c1eb2f54ed68c5fde93cbc8ace54bda6922b3f0b
Amine A <amine-98@live.fr>
2019-05-09 05:28:38 +0200

    MAJ des tests pour le nouveau format de paramètre

--- test_Gomoku.py
+++ test_Gomoku.py
@@ -7,12 +7,13 @@ def test_verif_tour3():
     grille[1][3] = 2
     grille[7][7] = 1
     # Grille au 3e tour
+
     assert verif_tour3(grille, (1, 3)) == False
     assert verif_tour3(grille, (6, 4)) == False
     assert verif_tour3(grille, (0, 0)) == True
 
 
 def test_conversion_pos_coord():
-    assert conversion_pos_coord(("A", 4)) == (0, 3)
-    assert conversion_pos_coord(("B", 7)) == (1, 6)
-    assert conversion_pos_coord(("O", 1)) == (14, 0)
+    assert conversion_pos_coord("A4") == (0, 3)
+    assert conversion_pos_coord("B7") == (1, 6)
+    assert conversion_pos_coord("O1") == (14, 0)
