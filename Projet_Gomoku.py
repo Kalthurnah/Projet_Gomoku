1d7825f00bd7bcfa85e08f093d1125c23a2f588b
Amine A <amine-98@live.fr>
2019-05-09 04:48:45 +0200

    Remonté une fonction au dessus du main, auto-indentation

--- Projet_Gomoku.py
+++ Projet_Gomoku.py
@@ -8,8 +8,9 @@ import numpy as np
 
 
 def creation_plateau():
-    plateau = np.zeros((15,15),dtype=int)#On crée une matrice 15x15 de 0
+    plateau = np.zeros((15, 15), dtype=int)  # On crée une matrice 15x15 de 0
     return plateau
+
 
 def demander_couleur():
     print("Les noirs commencent. Veux tu être :")
@@ -25,26 +26,28 @@ def demander_couleur():
         IA_char = 1
     return (IA_char, user_char)
 
+
+def verif_tour3(grille, coordonnees):
+    res = True
+    (i, j) = coordonnees
+    # La position est déjà converti en coordonnées dans la grille
+    if grille[i][j] != 0:
+        res = False
+    else:
+        # On vérifie la distance au centre de coordonnées (7,7)
+        distance = abs(7 - i) + abs(7 - j)
+        if distance < 7:
+            res = False
+    return res
+
+
 def Gomoku():
     print(user_char)
-    #Fonctionnement du Gomoku ici
+    # Fonctionnement du Gomoku ici
+
 
 if __name__ == '__main__':
     # Appeler main ici
     (IA_char, user_char) = demander_couleur()
     Gomoku()
-
-
-def verif_tour3(grille, coordonnees):
-    res = True
-    (i,j) = coordonnees
-    # La position est déjà converti en coordonnées dans la grille
-    if grille[i][j] != 0 :
-        res = False
-    else:
-        # On vérifie la distance au centre de coordonnées (7,7)
-        distance = abs(7-i)+abs(7-j)
-        if distance < 7 : 
-            res = False
-    return res
-
+# Ne pas mettre de fonctions ci dessous !
