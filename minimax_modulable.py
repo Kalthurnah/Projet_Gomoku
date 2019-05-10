810701b4b09230dfca469fee2975e60a9530a751
Amine A <amine-98@live.fr>
2019-05-11 01:01:51 +0200

    Correction d'un léger bug d'inégalité

--- minimax_modulable.py
+++ minimax_modulable.py
@@ -87,7 +87,7 @@ def minimax(grille_state, joueur, tour=0, profondeur=3, borne_min=-math.inf, bor
     Algorithme principal du minimax. Vérifier que les fonctions heuristic, terminal_test, actions
 
     :param grille_state: grille de l'état actuel du jeu
-    :param joueur: prochain joueur (dont on veut prédire la réaction)
+    :param joueur: joueur actuel du jeu (qui n'a pas encore joué)
     :param profondeur: maximum de profondeur de recherche du minimax
     :param tour: tour actuel du jeu, dans les jeux ou le tour influe sur les actions possibles
     :param borne_min:
@@ -105,7 +105,7 @@ def minimax(grille_state, joueur, tour=0, profondeur=3, borne_min=-math.inf, bor
             grille_state_action = result(grille_state, action, joueur)
             utility_action = minimax(grille_state_action, user_char, tour + 1, profondeur - 1, borne_min, borne_max)[0]
             new_maxi = max(utility_max, utility_action)  # nouveau maximum entre le maximum et l'utilité de cette action
-            if new_maxi != utility_max:  # Si le nouveau maximum est supérieur à l'ancien
+            if new_maxi >= utility_max:  # Si le nouveau maximum est supérieur à l'ancien
                 (utility_max, action_max) = (new_maxi, action)  # on met à jour l'utilité max et son action associée
 
             # Elagage alpha-beta v
@@ -128,7 +128,7 @@ def minimax(grille_state, joueur, tour=0, profondeur=3, borne_min=-math.inf, bor
             grille_state_action = result(grille_state, action, joueur)
             utility_action = minimax(grille_state_action, IA_char, tour + 1, profondeur - 1, borne_min, borne_max)[0]
             new_mini = min(utility_min, utility_action)  # nouveau minimum entre le minimum et l'utilité de cette action
-            if new_mini != utility_min:  # Si le nouveau minimum est inférieur à l'ancien,
+            if new_mini <= utility_min:  # Si le nouveau minimum est inférieur à l'ancien,
                 (utility_min, action_min) = (new_mini, action)  # on met à jour l'utilité min et son action associée
 
             # Elagage alpha-beta v
